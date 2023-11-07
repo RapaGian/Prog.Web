68c21f1a38834fc01542730036be5acd23fe477d
Gian Rapanaro <rapagiann@gmai.com>
2023-11-06 21:23:25 -0300

    AGREGADO M3U3 SIN TERMINAR

---
+++ M3 U3/routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post ('/', async (req, res, next) => { 
+  
+  console.log(req.body)
+
+  var nombre = req.body.nombre;
+  var apellido = req.body.apellido;
+  var email = req.body.email;
+  var tel = req.body.tel;
+  var mensaje = req.body.mensaje;
+
+  var obj ={
+    to:
+
+  }
+
+}
+
+)
+module.exports = router;
